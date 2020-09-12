b8fd64881884e55fd8b8c0d443d2eeefe520d344
Guilherme Vinicius Barbosa <guilherme.barbosa@fri.to>
2020-09-12 16:04:47 -0300

    .

--- ExemploIntents/app/src/main/java/br/gvb/exemplointents/ResultDetailActivity.kt
+++ ExemploIntents/app/src/main/java/br/gvb/exemplointents/ResultDetailActivity.kt
@@ -1,11 +1,34 @@
 package br.gvb.exemplointents
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_result_detail.*
 
 class ResultDetailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result_detail)
+
+        btnOp1.setOnClickListener {
+            responder("FIFA")
+        }
+
+        btnOp2.setOnClickListener {
+            responder("GTA")
+        }
+
+        btnOp3.setOnClickListener {
+            responder("COD")
+        }
+    }
+
+    fun responder(jogo: String){
+        val returnIntent = Intent()
+
+        returnIntent.putExtra("jogo", jogo)
+        setResult(Activity.RESULT_OK, returnIntent)
+        finish()
     }
 }
--- ExemploIntents/app/src/main/java/br/gvb/exemplointents/ResultMainActivity.kt
+++ ExemploIntents/app/src/main/java/br/gvb/exemplointents/ResultMainActivity.kt
@@ -1,11 +1,28 @@
 package br.gvb.exemplointents
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_result_main.*
 
 class ResultMainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result_main)
+
+        btnSelecionar.setOnClickListener {
+            val i = Intent(this, ResultDetailActivity::class.java)
+            startActivityForResult(i, 1)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if(requestCode == 1 && resultCode == Activity.RESULT_OK){
+            val jogo = data?.getStringExtra("jogo")
+            txtSelecao.setText(jogo)
+        }
     }
 }
