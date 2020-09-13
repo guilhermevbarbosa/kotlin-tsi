35622d76b51750d500f39c46348761234d7d6394
Guilherme Vinicius Barbosa <guilherme.barbosa@fri.to>
2020-09-12 22:58:53 -0300

    exemplo de fragmento

---
+++ ExemploFragments/app/src/main/java/br/gvb/exemplofragments/FragEstaticoActivity.kt
@@ -0,0 +1,11 @@
+package br.gvb.exemplofragments
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FragEstaticoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_frag_estatico)
+    }
+}
